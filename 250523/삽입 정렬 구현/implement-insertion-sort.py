a0b9d9e51fd8859c909d357f95bfcf309b6b362e
Min Seunggi <130447111+Sololus2431@users.noreply.github.com>
2025-05-23 22:14:40 +0900

    Title: 삽입 정렬 구현, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250523/삽입 정렬 구현/implement-insertion-sort.py
+++ 250523/삽입 정렬 구현/implement-insertion-sort.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-for i in range(1, len(arr)-1):
+for i in range(1, len(arr)):
     j = i-1
     key = arr[i]
     while j >=0  and arr[j] > key:
