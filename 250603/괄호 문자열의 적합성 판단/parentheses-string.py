73ad9f98c93820e19ccf5b2d89141bddc7db6f9b
Min Seunggi <130447111+Sololus2431@users.noreply.github.com>
2025-06-03 16:30:38 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 73ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250603/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -0,0 +1,44 @@
+str = input()
+
+# Please write your code here.
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def size(self):
+        return len(self.items)
+    
+    def empty(self):
+        return not self.items
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop():
+        if self.empty():
+            return
+        else:
+            self.items.pop()
+    
+    def top():
+        if self.empty()
+            return None
+        else:
+            return self.items[-1]
+
+
+s = Stack()
+for i in str:
+    if i == '(':
+        s.push()
+    else:
+        if s.empty():
+            print("No")
+            break
+        else:
+            s.pop()
+
+if s.empty():
+    print("Yes")
+else:
+    print("No")
